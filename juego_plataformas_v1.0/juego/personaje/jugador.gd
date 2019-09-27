03200a8475b423791ae1ae77607a38d3164e779b
brecabre <bravomantonio@gmail.com>
2019-09-27 22:28:11 +0200

    Add files via upload

--- juego_plataformas_v1.0/juego/personaje/jugador.gd
+++ juego_plataformas_v1.0/juego/personaje/jugador.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export (int) var run_speed = 200
+export (int) var run_speed = 300
 export (int) var jump_speed = -750
 export (int) var gravity = 1200
 
@@ -14,7 +14,7 @@ func get_input():
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
 	var jump = Input.is_action_just_pressed('ui_select')
-	#var arriba = Input.is_action_just_pressed('ui_up')
+#	var arriba = Input.is_action_just_pressed('ui_up')
 	var abajo = Input.is_action_pressed('ui_down')
 	if jump and is_on_floor() and not abajo:
         jumping = true
@@ -34,7 +34,7 @@ func _physics_process(delta):
     velocity = move_and_slide(velocity, Vector2(0, -1))
 
 func desactivar_y_activar_collision():
-	$CollisionJugadors.et_disabled(true) 
+	$CollisionJugador.set_disabled(true) 
 	$Tiempo_activar_collision.start()
 func _on_Tiempo_activar_collision_timeout():
-	$CollisionJugadors.et_disabled(false)
+	$CollisionJugador.set_disabled(false)
