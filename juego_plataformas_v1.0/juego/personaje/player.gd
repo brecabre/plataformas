2aff0e3efad465dda1a824fab2c3c8f64470b338
brecabre <bravomantonio@gmail.com>
2019-10-02 17:43:09 +0200

    cuestas
    cuestas parado

--- juego_plataformas_v1.0/juego/personaje/player.gd
+++ juego_plataformas_v1.0/juego/personaje/player.gd
@@ -1,22 +1,19 @@
 extends KinematicBody2D
 
-export (int) var run_speed = 30
-export (int) var jump_speed = -250
-export (int) var gravity = 420
+export (int) var run_speed = 100
+export (int) var jump_speed = -500
+export (int) var gravity = 1200
 
 var velocity = Vector2()
 var jumping = false
-func _ready():
-	
-	pass
-	
+
 func get_input():
 	velocity.x = 0
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
 	var jump = Input.is_action_just_pressed('ui_select')
-#	var arriba = Input.is_action_just_pressed('ui_up')
 	var abajo = Input.is_action_pressed('ui_down')
+	
 	if jump and is_on_floor() and not abajo:
         jumping = true
         velocity.y = jump_speed
@@ -24,24 +21,23 @@ func get_input():
         velocity.x += run_speed
 	if left:
         velocity.x -= run_speed
-#	if abajo and jump:
-#		activar_desactivar_colision()
-#	if is_on_floor() and not (jump or abajo or right or left) :
-#		velocity=Vector2(0,0)
-#
-#		print("sssiiiiiii")
-#	if	not is_on_floor():
-#		print("nonononnnnnnnnno")
+	if abajo and jump:
+		activar_desactivar_colision()
 func _physics_process(delta):
+	
 	get_input()
-	
-	
-	velocity.y += gravity * delta
+#	print (jumping)
+	# para que no se resbale por las cuestas
+	if is_on_floor() and velocity.x == 0 and jumping == false:
+		velocity.y = 0	
+	else:	
+		velocity.y += gravity * delta
 	if jumping and is_on_floor():
-        jumping = false
-		
+		jumping = false
+	
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 #	velocity = move_and_slide_with_snap(velocity, Vector2(0, -1), Vector2(0, 32))
+
 func activar_desactivar_colision():
 	set_collision_layer_bit(0,false)
 	set_collision_mask_bit(0,false)
