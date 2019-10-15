575e539c3eceb0133c6ad5a34a69ca7ba8f4727e
brecabre <bravomantonio@gmail.com>
2019-10-15 20:25:19 +0200

    movimiento y aturdimiento

--- juego_plataformas_v1.0/juego/enemigos/enemigo1/enemigo1.1.gd
+++ juego_plataformas_v1.0/juego/enemigos/enemigo1/enemigo1.1.gd
@@ -6,24 +6,42 @@ const SPEED = 200
 var velocity = Vector2()
 var left = Vector2(-1, 0)
 var right = Vector2(1, 0)
-
-var direction  = left
+var parado = Vector2(0,0)
+var aturdido = false
+var antiguadireccion
+var direction = left
 export var snap := true
 
 export var slope_slide_threshold := 50.0
 
 func _physics_process(delta):
-	
+#	print(randi()%2+1)
 	velocity.y += gravity * delta
 	velocity.x = direction.x * SPEED
 	var snap_vector = Vector2(0,32) if snap else Vector2()
 	velocity = move_and_slide_with_snap(velocity,snap_vector, Vector2.UP, slope_slide_threshold)
-	if is_on_wall():
+	var nueva = null
+	if aturdido:
+		estaaturdido()
+	else:
+		moverse() 
+
+func moverse():
+	
+	if is_on_wall() and not aturdido:
 		if direction == left:
 			direction = right
 			$Sprite.set_flip_h(true)
 		elif direction == right:
 			direction = left
 			$Sprite.set_flip_h(false)
-		
-				
+	if direction == parado :
+		var random = randi()%2+1
+		if random == 1:
+			direction = left
+		else:
+			direction = right
+func estaaturdido():
+#	antiguadireccion = direction
+	direction = parado
+	
--- juego_plataformas_v1.0/juego/pistola/aspiradora/aspiradora.gd
+++ juego_plataformas_v1.0/juego/pistola/aspiradora/aspiradora.gd
@@ -8,11 +8,24 @@ func _ready():
 	set_process(true)
 	set_process_input(true)
 
-func _input(event):
-	if(Input.is_action_pressed("disparar")):
-#		AreaColision.set_disabled(false) 
-		print("absorver")
+#func _input(event):
+#	if(Input.is_action_pressed("disparar")):
+##		AreaColision.set_disabled(false) 
+#		print("absorver")
+#
+#	if(event.is_action_released("disparar")):
+##		AreaColision.set_disabled(true) 
+#		print("disparar")
+
+func _on_AreaAspira_area_entered(area):
+	if area.is_in_group("enemigo"):
+		area.get_parent().aturdido = true
+		
+		
+
+
+func _on_AreaAspira_area_exited(area):
+	if area.is_in_group("enemigo"):
+		var _stop = Vector2(0,0)
+		area.get_parent().aturdido = false
 	
-	if(event.is_action_released("disparar")):
-#		AreaColision.set_disabled(true) 
-		print("disparar")
