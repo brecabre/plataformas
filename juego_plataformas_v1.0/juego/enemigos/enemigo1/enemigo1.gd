127becd392ba35bfe7e568ca87e38df63c171f07
brecabre <bravomantonio@gmail.com>
2019-10-29 01:22:31 +0100

    por fin arreglo lo del connect

--- juego_plataformas_v1.0/juego/enemigos/enemigo1/enemigo1.gd
+++ juego_plataformas_v1.0/juego/enemigos/enemigo1/enemigo1.gd
@@ -14,21 +14,22 @@ export var snap := true
 
 export var slope_slide_threshold := 50.0
 
+
 func _physics_process(delta):
 
 	velocity.y += gravity * delta
 	velocity.x = direction.x * SPEED
 	var snap_vector = Vector2(0,32) if snap else Vector2()
 	velocity = move_and_slide_with_snap(velocity,snap_vector, Vector2.UP, slope_slide_threshold)
-
+	
 	update_animation(velocity)
 
 	if aspirado:
 		aspirando()
-
+		
 	elif aspirado == false:
 		moverse()
-		$Timeraspirar.stop()
+#		
 
 func update_animation(velocity: Vector2) -> void:
 
@@ -38,11 +39,16 @@ func update_animation(velocity: Vector2) -> void:
 
 		if aspirado:
 			animation = "animaspirado"
+			#compruebo primero si se a activado la señal para evitar errores
+			if animation_player.is_connected( "animation_finished", self, "aspirados")==false:
+				# conecto el final de la animacion con la func aspirados
+				animation_player.connect("animation_finished",self,"aspirados")
+#			
 		else:
 			animation = "andar"
 
 		animation_player.play(animation)
-
+		
 func moverse():
 	snap = true
 	gravity = 300
@@ -65,30 +71,29 @@ func moverse():
 
 func aspirando():
 
-
+	
 	var nodojugador = get_owner().get_parent().get_node("jugador")
 	var posicion_aspiradora = nodojugador.get_node("aspiradora/Position2D").get_global_position()
 	var direccionaspirar = (posicion_aspiradora - get_global_position()).normalized()
 	$CollisionShape2D.set_disabled(true)
 	snap = false
 	gravity = 0
-
+#	
 	if posicion_aspiradora != get_global_position():
 
 		direction.x = direccionaspirar.x
 		velocity.y = direccionaspirar.y*SPEED
+	
+
+func _on_AreaDanoEnemigo1_area_entered(area):
+	if area.is_in_group("proyectil"):
+		queue_free()
 
 
+func aspirados(animaspirado):
 
-func _on_Timeraspirar_timeout():
 	var nodojugador = get_owner().get_parent().get_node("jugador")
 	var aspiradora = nodojugador.get_node("aspiradora")
 	aspiradora.lista_aspi.append($Sprite.texture.get_path())
 
 	queue_free()
-	pass # Replace with function body.
-
-
-func _on_AreaDanoEnemigo1_area_entered(area):
-	if area.is_in_group("proyectil"):
-		queue_free()
